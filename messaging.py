4101d9ad6d29984211f058238dce31455aba8c56
Ivan Sychev <i77599146613@yandex.ru>
2023-04-04 11:46:08 +0500

    Initial commit

---
+++ messaging.py
@@ -0,0 +1,153 @@
+import telebot
+import logging
+import xlwt
+import xlrd
+from time import sleep
+
+ADMIN_ID = 532081536
+bot = telebot.TeleBot('',skip_pending=True)
+logging.basicConfig(filename="logs.log", level=logging.INFO, format=' %(asctime)s - %(levelname)s - %(message)s')
+allowedusers = [[0, 0]]
+ur = xlrd.open_workbook("users.xls")
+sheetr = ur.sheet_by_index(0)
+userslist = sheetr.col_values(0)
+grouplist = sheetr.col_values(1)
+for i in range(len(userslist)):
+    a = int(userslist[i])
+    b = int(grouplist[i])
+    allowedusers.append([a, b])
+logging.info("start bot")
+
+
+@bot.message_handler(content_types=['text'])
+def start(message):
+    logging.info("user message:" + message.text + ":" + str(message.from_user.first_name) + " " + str(
+        message.from_user.last_name) + ":" + str(
+        message.from_user.username) + ":" + str(message.from_user.id))
+    if message.text == "/start" and not (
+            ([message.from_user.id, 1] in allowedusers) or ([message.from_user.id, 2] in allowedusers)):
+        keyboard = telebot.types.InlineKeyboardMarkup()
+        key_g1 = telebot.types.InlineKeyboardButton(text='1', callback_data='change group 1')
+        key_g2 = telebot.types.InlineKeyboardButton(text='2', callback_data='change group 2')
+        key_cancel = telebot.types.InlineKeyboardButton(text='отмена', callback_data='cancel')
+        keyboard.add(key_g1, key_g2)
+        keyboard.add(key_cancel)
+        bot.send_message(message.from_user.id,
+                         text="Привет, когда закончится пара этот бот напомнит, какая следующая, чтобы тебе не "
+                              "пришлось искать расписание. Кроме того, бот постоянно обновляет расписание, "
+                              "поэтому ты будешь в курсе изменений.\n" + "Если хочешь зарегестрироваться, укажи группу",
+                         reply_markup=keyboard)
+    elif message.text == "/start":
+        bot.send_message(message.from_user.id, "Вы уже зарегестрированы")
+    elif message.text == "/users" and message.from_user.id == ADMIN_ID:
+        bot.send_document(ADMIN_ID, open("users.txt", 'rb'))
+    elif message.text == "/logs" and message.from_user.id == ADMIN_ID:
+        bot.send_document(ADMIN_ID, open("logs.log", 'rb'))
+    elif message.text == "/settings" and not (
+            ([message.from_user.id, 1] in allowedusers) or ([message.from_user.id, 2] in allowedusers)):
+        bot.send_message(message.from_user.id, "Вы еще не зарегестрированы")
+    elif message.text == "/settings":
+        u = "0"
+        for i in allowedusers:
+            if i[0] == message.from_user.id:
+                u = str(i[1])
+                break
+        keyboard = telebot.types.InlineKeyboardMarkup()
+        key_change = telebot.types.InlineKeyboardButton(text='изменить группу', callback_data='change')
+        key_delete = telebot.types.InlineKeyboardButton(text='удалить', callback_data='delete')
+        key_cancel = telebot.types.InlineKeyboardButton(text='отмена', callback_data='cancel')
+        keyboard.add(key_change)
+        keyboard.add(key_delete)
+        keyboard.add(key_cancel)
+        bot.send_message(message.from_user.id, reply_markup=keyboard,
+                         text="Ваша группа - " + u + "\n" + "Хотите изменить группу или удалить себя из списка?")
+
+
+@bot.callback_query_handler(func=lambda call: True)
+def callback_worker(call):
+    if call.data == "change":
+        bot.delete_message(call.message.chat.id, call.message.id)
+        keyboard = telebot.types.InlineKeyboardMarkup()
+        key_g1 = telebot.types.InlineKeyboardButton(text='1', callback_data='change group 1')
+        key_g2 = telebot.types.InlineKeyboardButton(text='2', callback_data='change group 2')
+        key_cancel = telebot.types.InlineKeyboardButton(text='отмена', callback_data='cancel')
+        keyboard.add(key_g1, key_g2)
+        keyboard.add(key_cancel)
+        bot.send_message(call.message.chat.id, text="Выберите группу", reply_markup=keyboard)
+        logging.info(
+            "user changing:" + str(call.message.chat.first_name) + " " + str(call.message.chat.last_name) + ":" + str(
+                call.message.chat.username) + ":" + str(call.message.chat.id))
+    elif call.data == "delete":
+        bot.delete_message(call.message.chat.id, call.message.id)
+        r = 0
+        for i in range(len(allowedusers)):
+            if allowedusers[i][0] == call.message.chat.id:
+                r = i
+                break
+        allowedusers[r][1] = -2
+        rewrite_users()
+        bot.send_message(call.message.chat.id, "Готово")
+        logging.info(
+            "user deleted:" + str(call.message.chat.first_name) + " " + str(call.message.chat.last_name) + ":" + str(
+                call.message.chat.username) + ":" + str(call.message.chat.id))
+    elif call.data == "cancel":
+        bot.delete_message(call.message.chat.id, call.message.id)
+        bot.send_photo(call.message.chat.id, open("ok.png", 'rb'))
+        logging.info(
+            "user canceled:" + str(call.message.chat.first_name) + " " + str(call.message.chat.last_name) + ":" + str(
+                call.message.chat.username) + ":" + str(call.message.chat.id))
+    elif call.data == "change group 1":
+        bot.delete_message(call.message.chat.id, call.message.id)
+        r = -1
+        for i in range(len(allowedusers)):
+            if allowedusers[i][0] == call.message.chat.id:
+                r = i
+                break
+        if r == -1:
+            allowedusers.append([call.message.chat.id, 1])
+        else:
+            allowedusers[r][1] = 1
+        rewrite_users()
+        bot.send_message(call.message.chat.id, "Готово")
+        logging.info("user chose group 1:" + str(call.message.chat.first_name) + " " + str(
+            call.message.chat.last_name) + ":" + str(call.message.chat.username) + ":" + str(call.message.chat.id))
+    elif call.data == "change group 2":
+        bot.delete_message(call.message.chat.id, call.message.id)
+        r = -1
+        for i in range(len(allowedusers)):
+            if allowedusers[i][0] == call.message.chat.id:
+                r = i
+                break
+        if r == -1:
+            allowedusers.append([call.message.chat.id, 2])
+        else:
+            allowedusers[r][1] = 2
+        rewrite_users()
+        bot.send_message(call.message.chat.id, "Готово")
+        logging.info(
+            "user chose group 2:" + str(call.message.chat.first_name)+" " + str(call.message.chat.last_name) + ":" + str(
+                call.message.chat.username) + ":" + str(call.message.chat.id))
+
+
+def rewrite_users():
+    l = len(allowedusers)
+    i = 1
+    uw = xlwt.Workbook(encoding="utf8")
+    sheetw = uw.add_sheet("users")
+    while i < l:
+        if allowedusers[i][1] != -2:
+            sheetw.write(i - 1, 0, allowedusers[i][0])
+            sheetw.write(i - 1, 1, allowedusers[i][1])
+            i += 1
+        else:
+            del allowedusers[i]
+            l -= 1
+    uw.save("users.xls")
+
+
+while True:
+    try:
+        bot.polling(none_stop=True, interval=1)
+    except Exception as e:
+        logging.error("network trouble")
+        sleep(15)
---
+++ sending.py
@@ -0,0 +1,185 @@
+import telebot
+import xlrd
+import os
+from time import sleep
+import datetime
+import wget
+import logging
+
+bot = telebot.TeleBot('')
+logging.basicConfig(filename="logs.log", level=logging.INFO, format=' %(asctime)s - %(levelname)s - %(message)s')
+
+LINK = "https://docs.google.com/spreadsheets/d/1tGbeevMu_7_n_pKDFjH3cNFNigClVW3v/export?format=xlsx&id=1tGbeevMu_7_n_pKDFjH3cNFNigClVW3v"
+
+A1 = [["" for i in range(2)] for j in range(4)]
+A2 = [["" for k in range(2)] for l in range(4)]
+allowedusers = [[0, 0]]
+
+
+def update_schedule():
+    if os.path.isfile("Schedule.xlsx"):
+        os.remove("Schedule.xlsx")
+    t = True
+    while t:
+        try:
+            wget.download(LINK, "Schedule.xlsx")
+            logging.info("schedule downloaded")
+            t = False
+        except Exception:
+            logging.error("schedule download failed")
+            sleep(5)
+            t = True
+
+
+def update_users():
+    ur = xlrd.open_workbook("users.xls")
+    sheet_r = ur.sheet_by_index(0)
+    n = sheet_r.nrows
+    del allowedusers[1:]
+    for i in range(n):
+        a = int(sheet_r.row_values(i)[0])
+        b = int(sheet_r.row_values(i)[1])
+        allowedusers.append([a, b])
+    logging.info("users updated")
+
+
+def isMerged(r, c):
+    wb1 = xlrd.open_workbook("Schedule.xlsx")
+    sheet1 = wb1.sheet_by_index(0)
+    m = sheet1.merged_cells
+    return (r, r + 1, c - 1, c + 1) in m
+
+
+def get_schedule(weekday):
+    wb = xlrd.open_workbook("Schedule.xlsx")
+    sheet = wb.sheet_by_index(0)
+    if weekday >= 1:
+        x = 1
+    else:
+        x = 0
+    for i in range(4):
+        k = sheet.row_values(3 + 5 * weekday + i + x)[60]
+        if isMerged(3 + weekday * 5 + i + x, 59):
+            l = sheet.row_values(3 + weekday * 5 + i + x)[58]
+            if l.find("(") == -1:
+                A1[i][0] = l.strip()
+                A2[i][0] = l.strip()
+            else:
+                A1[i][0] = l[:l.find("(")].strip()
+                A2[i][0] = l[:l.find("(")].strip()
+            if type(k) == float:
+                k = str(k)[:-2]
+            A1[i][1] = k
+            A2[i][1] = k
+        else:
+            l1 = sheet.row_values(3 + weekday * 5 + i + x)[58]
+            l2 = sheet.row_values(3 + weekday * 5 + i + x)[59]
+            if l1.find("(") == -1:
+                A1[i][0] = l1.strip()
+            else:
+                A1[i][0] = l1[:l2.find("(")].strip()
+            if l2.find("(") == -1:
+                A2[i][0] = l2.strip()
+            else:
+                A2[i][0] = l2[:l2.find("(")].strip()
+            if type(k) == float:
+                k = str(k)[:-2]
+            if k.find("/") != -1:
+                A1[i][1] = k[:k.find("/")]
+                A2[i][1] = k[k.find("/") + 1:]
+            elif A1[i][0] != "" and A2[i][0] == "":
+                A1[i][1] = k
+                A2[i][1] = ""
+            elif A1[i][0] == "" and A2[i][0] != "":
+                A1[i][1] = ""
+                A2[i][1] = k
+            else:
+                A1[i][1] = k
+                A2[i][1] = k
+            if A1[i][0] == "":
+                A1[i][1] = "---"
+            if A2[i][0] == "":
+                A2[i][1] = "---"
+
+    logging.info("Schedule updated")
+
+
+def send_next_lesson(g1, g2):
+    if g1[0] != "":
+        for i in allowedusers:
+            if i[1] == 1:
+                bot.send_message(i[0], g1[0] + " в " + g1[1])
+            logging.info("lesson sended to" + str(i[0]))
+    if g2[0] != "":
+        for i in allowedusers:
+            if i[1] == 2:
+                bot.send_message(i[0], g2[0] + " в " + g2[1])
+            logging.info("lesson sended to" + str(i[0]))
+
+
+def send_schedule():
+    now = datetime.datetime.now()
+    current_time = now.strftime("%H:%M")
+    weekday = now.weekday()
+    if weekday > 4:
+        sleep(3600 * 5)
+    else:
+        if current_time == "06:30":
+            update_schedule()
+            get_schedule(weekday)
+            update_users()
+            for i in allowedusers[1:]:
+                if i[1] == 1:
+                    bot.send_message(i[0],
+                                     "Расписание на сегодня:\n1. " + A1[0][0] + " [" + A1[0][
+                                         1] + "]\n2. " +
+                                     A1[1][0] + " [" + A1[1][1] + "]\n3. " + A1[2][0] + " [" +
+                                     A1[2][1] + "]\n4. "
+                                     + A1[3][0] + " [" + A1[3][1] + "]\n")
+                elif i[1] == 2:
+                    bot.send_message(i[0],
+                                     "Расписание на сегодня:\n1. " + A2[0][0] + " [" + A2[0][
+                                         1] + "]\n2. " +
+                                     A2[1][0] + " [" + A2[1][1] + "]\n3. " + A2[2][0] + " [" +
+                                     A2[2][1] + "]\n4. "
+                                     + A2[3][0] + " [" + A2[3][1] + "]\n")
+                logging.info("morning schedule sended to" + str(i[0]))
+            sleep(60)
+        elif current_time == "08:50":
+            update_schedule()
+            get_schedule(weekday)
+            update_users()
+            g1 = A1[0]
+            g2 = A2[0]
+            send_next_lesson(g1, g2)
+            sleep(60)
+        elif current_time == "10:30":
+            update_schedule()
+            get_schedule(weekday)
+            update_users()
+            g1 = A1[1]
+            g2 = A2[1]
+            send_next_lesson(g1, g2)
+            sleep(60)
+        elif current_time == "12:20":
+            update_schedule()
+            get_schedule(weekday)
+            update_users()
+            g1 = A1[2]
+            g2 = A2[2]
+            send_next_lesson(g1, g2)
+            sleep(60)
+        elif current_time == "14:10":
+            update_schedule()
+            get_schedule(weekday)
+            update_users()
+            print("14:10", allowedusers)
+            g1 = A1[3]
+            g2 = A2[3]
+            send_next_lesson(g1, g2)
+            sleep(60)
+
+
+while True:
+    send_schedule()
+    sleep(30)
