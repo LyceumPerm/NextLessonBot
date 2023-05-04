6779193c0d156ce65dd10b206c89e81bd622c9a4
iveniuss <i77599146613@yandex.ru>
2023-05-05 00:40:23 +0500

    delete odd +

--- messaging.py
+++ messaging.py
@@ -158,7 +158,7 @@ def rewrite_users():
 
 def log_message(m):
     logging.info(
-        f"user message: {m.text} : {m.from_user.first_name} {m.from_user.last_name} : {m.from_user.username} : + {m.from_user.id}")
+        f"user message: {m.text} : {m.from_user.first_name} {m.from_user.last_name} : {m.from_user.username} : {m.from_user.id}")
 
 
 while True:
